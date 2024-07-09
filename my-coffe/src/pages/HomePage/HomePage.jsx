c08ca20d5d62f30413083acc16f75b27ea6479d1
2vasy <2vasy@mail.ru>
2024-07-09 14:27:44 +0200

    Homepage_building_HTML_SCSS

--- my-coffe/src/pages/HomePage/HomePage.jsx
+++ my-coffe/src/pages/HomePage/HomePage.jsx
@@ -1,11 +1,12 @@
 
 import CoffeBeans from "../../components/UI/coffeeBeans3";
-import CardC from "../../assets/img/cards/815O9ktyfUL.png"
+import Solimo from "../../assets/img/cards/815O9ktyfUL.png"
+import Presto from "../../assets/img/cards/91Ryk2gKejL.png"
+import Aromistico from "../../assets/img/cards/71qBQnpQFYL.png"
 import "./HomePage.scss";
 function HomePage() {
   return (
-    <>
-      <main>
+      <>
         <h1 className="title_about">About Us</h1>
         <CoffeBeans color="black" />
         <div className="text_about">
@@ -27,31 +28,41 @@ function HomePage() {
             ladies now.
           </p>
         </div>
-        <div className="cards_items">
-          <div className="cards_items_title">Our best</div>
-          <div className="cards_wrapper">
-            <figure className="figure">
-              <img src={CardC} className="figure-img img-fluid rounded" alt="..." />
-              <figcaption className="figure-caption text-right">
-                A caption for the above image.
-              </figcaption>
-            </figure>
-            <figure className="figure">
-              <img src={CardC} className="figure-img img-fluid rounded" alt="..." />
-              <figcaption className="figure-caption text-right">
-                A caption for the above image.
-              </figcaption>
-            </figure>
-            <figure className="figure">
-              <img src={CardC} className="figure-img img-fluid rounded" alt="..." />
-              <figcaption className="figure-caption text-right">
-                A caption for the above image.
-              </figcaption>
-            </figure>
+        <section className="cards_section">
+          <div className="cards_items">
+            <h1 className="cards_items_title">Our best</h1>
+            <div className="cards_wrapper">
+              <figure className="figure">
+                <img src={Solimo} className="figure-img img-fluid rounded" alt="..." />
+                <figcaption className="figure-caption text-right">
+                  Solimo Coffee Beans 2 kg
+                </figcaption>
+                <div className="price_container">
+                  <div className="price">10.73$</div>
+                </div>
+              </figure>
+              <figure className="figure">
+                <img src={Presto} className="figure-img img-fluid rounded" alt="..." />
+                <figcaption className="figure-caption text-right">
+                  Presto Coffee Beans 1 kg
+                </figcaption>
+                <div className="price_container">
+                  <div className="price">15.99$</div>
+                </div>
+              </figure>
+              <figure className="figure">
+                <img src={Aromistico} className="figure-img img-fluid rounded" alt="..." />
+                <figcaption className="figure-caption text-right">
+                  AROMISTICO Coffee 1 kg  
+                </figcaption>
+                <div className="price_container">
+                  <div className="price">6.99$</div>
+                </div>
+              </figure>
+            </div>
           </div>
-        </div>
-      </main>
-    </>
+        </section>
+      </>
   );
 }
 
